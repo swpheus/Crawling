6db14044b52c153172e6c5949f8bd20af8f5f7a7
sungwoo woo <swpheus1@naver.com>
2018-07-27 14:53:51 +0900

    modlfy

--- crawling.py
+++ crawling.py
@@ -4,8 +4,8 @@ from selenium import webdriver
 driver =webdriver.Chrome('C:/Users\swphe\Downloads\chromedriver')
 driver.get("https://nid.naver.com/nidlogin.login")
 
-driver.find_element_by_name('id').send_keys('swpheus1')
-driver.find_element_by_name('pw').send_keys('sw846200$$')
+driver.find_element_by_name('id').send_keys('ID')
+driver.find_element_by_name('pw').send_keys('PW')
 
 driver.find_element_by_xpath('//*[@id="frmNIDLogin"]/fieldset/input').click()
 
